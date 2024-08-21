dc678734aebd2d69b08f6b6fe2e8dabedd6dabac
David Salcedo <99992909+david03130@users.noreply.github.com>
2024-08-21 19:26:32 +0200

    change: Update Guitar with cart callback

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,20 @@ import { db } from './data/db';
 
 function App() {
   const [data, setData] = useState(db);
+  const [cart, setCart] = useState([]);
+
+  function handleAddToCart(item) {
+    const existingItem = cart.find((x) => x.item.id === item.id);
+
+    if (!existingItem) {
+      setCart(prevCart => [...prevCart, { item: item, quantity: 1 }]);
+    }
+    else {
+      // There's no point in cloning the array because the reference of the objects persists. 
+      // The only thing that becomes a shallow copy is the actual array object.
+      existingItem.quantity++;
+    }
+  }
 
   return (
     <>
@@ -14,7 +28,7 @@ function App() {
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-          {data.map((x) => (<Guitar key={x.id} guitar={x} />))}
+          {data.map((x) => (<Guitar key={x.id} guitar={x} onAddToCart={handleAddToCart} />))}
         </div>
       </main>
 
--- src/components/Guitar/guitar.jsx
+++ src/components/Guitar/guitar.jsx
@@ -1,4 +1,10 @@
-export default function Guitar({ guitar: { id, name, description, price, image } }) {
+export default function Guitar({ guitar, onAddToCart }) {
+    const { id, name, description, price, image } = guitar;
+
+    function handleAddToCartOnClick(guitar) {
+        onAddToCart(guitar)
+    }
+
     return (
         <div className="col-md-6 col-lg-4 my-4 row align-items-center">
             <div className="col-4">
@@ -8,7 +14,7 @@ export default function Guitar({ guitar: { id, name, description, price, image }
                 <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
                 <p>{description}</p>
                 <p className="fw-black text-primary fs-3">{price}</p>
-                <button type="button" className="btn btn-dark w-100">Agregar al Carrito</button>
+                <button onClick={() => handleAddToCartOnClick(guitar)} type="button" className="btn btn-dark w-100">Agregar al Carrito</button>
             </div>
         </div>
     )
